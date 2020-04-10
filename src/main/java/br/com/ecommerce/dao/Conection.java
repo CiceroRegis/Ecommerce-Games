ff88083cc3c2bdb70aa68a2ebe2a36b88e411226
cicero.cruz <ciceroregis25@gmail.com>
2020-04-10 01:37:34 -0300

    create conection DB

---
+++ src/main/java/br/com/ecommerce/dao/Conection.java
@@ -0,0 +1,37 @@
+package br.com.ecommerce.dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class Conection {
+	
+	private static String driver = "com.mysql.jdbc.Driver";
+	private static String usuario = "root";
+	private static String senha = "root";
+	private static String url = "jdbc:mysql://localhost:3306/sgp?useSSL=false";
+
+	
+	public static  Connection getConnection() throws SQLException{
+		Connection conn = null;		
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, usuario, senha);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	public static void closeConnection(Connection conn, PreparedStatement pst, ResultSet rs){
+		try {
+			if(pst != null){pst.clearParameters(); pst.close();}
+			if(rs != null){rs.close();}
+			if(conn != null){conn.close();}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/test/java/br/com/ecommerce/testes/TestaConexao.java
@@ -0,0 +1,5 @@
+package br.com.ecommerce.testes;
+
+public class TestaConexao {
+
+}
